e95c3e078d27c2c5e8c7ab9cb0705325244f8065
Aymeric <36987924+Aym02@users.noreply.github.com>
2019-06-20 12:25:44 +0200

    Ajout de la fonction toLowerCase() dans le champ de recherche pour pouvoir écrire avec ou sans Majuscules

--- projet/src/app/filter.pipe.ts
+++ projet/src/app/filter.pipe.ts
@@ -7,13 +7,15 @@ import { Annonce } from './annonce';
 
 export class FilterPipe implements PipeTransform {
   transform(items: Annonce[], searchText: string): any[] {
+    // S'il n'y a pas d'items on ne renvoie rien
     if (!items) {return []; }
+    // S'il n'y a pas de searchText pour trier a.k.a. pas de texte dans le champ recherche, on renvoie tous les items
     else if (!searchText) {return items; } else {
-
+      // Sinon on récupère les items dont le titres contiennent la valeur du texte searchText dans leur titre
       let annoncesFiltrees: Annonce[] = [];
 
       for (let i in items){
-        if (items[i].title.includes(searchText)) {
+        if (items[i].title.toLowerCase().includes(searchText.toLowerCase())) {
           annoncesFiltrees.push(items[i]);
         }
       }
